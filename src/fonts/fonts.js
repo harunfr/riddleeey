1fbadc42f27bb09f8201ca61dcf030dff29f2487
harunfr <harun-626@hotmail.com>
2022-05-25 21:38:53 +0300

    added custom fonts

---
+++ src/fonts/fonts.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+import sibylAlternates from './Sibyl_Alternates.woff';
+import sibylAlternates2 from './Sibyl_Alternates.woff2';
+import hogBoldHMK from './Hog_Bold_HMK.woff';
+import hogBoldHMK2 from './Hog_Bold_HMK.woff2';
+
+export default createGlobalStyle`
+    @font-face {
+        font-family: 'Font Name';
+        src: local('Font Name'), local('FontName'),
+        url(${sibylAlternates2}) format('woff2'),
+        url(${sibylAlternates}) format('woff');
+        font-weight: 300;
+        font-style: normal;
+    }
+    @font-face {
+        font-family: 'Font Name2';
+        src: local('Font Name2'), local('FontName2'),
+        url(${hogBoldHMK2}) format('woff2'),
+        url(${hogBoldHMK}) format('woff');
+        font-weight: 300;
+        font-style: normal;
+    }
+`;
