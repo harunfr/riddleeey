d090b34cf82206242b09d4ac41adc6624a17fbcc
harunfr <harun-626@hotmail.com>
2022-05-26 16:35:07 +0300

    added basic keypress functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+
+import GlobalFonts from './fonts/fonts';
+import GlobalStyle from './styles/global'; // eslint-disable-next-line
+import Game from './Game/Game';
+
+import Header from './components/Header';
+import InputArea from './components/InputArea';
+import Riddle from './components/Riddle';
+import Keyboard from './components/Keyboard';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <GlobalStyle />
+      <GlobalFonts />
+      <Header />
+      <InputArea />
+      <Riddle />
+      <Keyboard />
+    </>
   );
 }
 
--- src/components/InputArea/index.tsx
+++ src/components/InputArea/index.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import Game from '../../Game/Game';
 import { Container, Row, Cell } from './styles';
 
 const game = new Game();
+game.answer = 'WORDLE';
 
 function InputArea() {
+  const cellsStackSnapShot = [...game.cellsStack];
+  const [cellsStack, setCellsStack] = useState(cellsStackSnapShot);
+
+  const keyDownHandler = (event: any) => {
+    if (event.key === 'Enter') {
+      game.makeAGuess();
+    } else if (event.key === 'Backspace') {
+      game.delete();
+    } else {
+      game.add(event.key);
+    }
+    setCellsStack(game.cellsStack.slice());
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', keyDownHandler);
+    return () => {
+      window.removeEventListener('keydown', keyDownHandler);
+    };
+  }, []);
+
   return (
     <Container>
-      {game.cellsStack.map((row) => (
+      {cellsStack.map((row) => (
         <Row key={uuidv4()}>
           {row.map((cell) => (
-            <Cell key={uuidv4()}>{cell.letter || 'A'}</Cell>
+            <Cell key={uuidv4()}>{cell.letter}</Cell>
           ))}
         </Row>
       ))}
